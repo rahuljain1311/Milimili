bb4f095b51e3c39bebe4791ee3d3b77d4e663b0d
Rahul Jain <rahul_jain@mckinsey.com>
2018-11-18 22:16:44 +0530

    Bug: Failed to execute 'send' on 'RTCDataChannel': RTCDataChannel.readyState is not 'open'

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ var servers = {
 };
 
 var peerConnections = {};
-var pc = {};
+var channel = {};
 
 /*************************** Config setup complete ************************************/
 
@@ -52,6 +52,48 @@ function shareICECandidates (){
         selectPlayersDropDown.appendChild(player);
 
         // TODO: We need to remove the users when they close the browser window 
+
+
+        // Offerer side
+        channel[receiverId] = peerConnections[receiverId].createDataChannel("milimili");
+        channel[receiverId].onopen = function(event) {
+          channel[receiverId].send('Player 1 ', myId);
+        }
+        channel[receiverId].onmessage = function(event) {
+
+            var object = JSON.parse(event.data);
+            console.log('A message received on Offerer side', object);
+            if(object.id){ // this will be my logic
+                if(object.id !== myId){
+                    document.getElementById('chat').appendChild(document.createElement('div'));
+                    document.getElementById("chat").lastChild.innerHTML += object.id + ': ' + object.message;
+                }
+            }
+            else 
+                console.log('Player1: ', event.data);
+        }
+
+
+        // // Answerer side
+        peerConnections[receiverId].ondatachannel = function(event) {
+            channel[receiverId] = event.channel;
+            channel[receiverId].onopen = function(event) {
+                channel[receiverId].send('Hi back from answerer!');
+            }
+            channel[receiverId].onmessage = function(event) {
+
+            var object = JSON.parse(event.data);
+            console.log('A message received on Answerer side', object, object.id);
+            if(object.id){ // this will be my logic
+                if(object.id != myId){
+                    document.getElementById('chat').appendChild(document.createElement('div'));
+                    document.getElementById("chat").lastChild.innerHTML += object.id + ': ' + object.message;
+                }
+            }
+            else 
+                console.log('Here in onmessage of Offerer', event.data);
+          }
+        }
     }
 }
 
@@ -111,90 +153,28 @@ function readMessage(data) {
 
 /*************************** ICE connection established ************************************/
 
+function chat() {
+    var message = document.getElementById("myInput").value;
 
-// var pc = new RTCPeerConnection(servers);
-// pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})) : console.log("Sent All Ice") );
+    var dropdownElement = document.getElementById("players");
+    console.log('#############receiverId = ', dropdownElement);
+    var receiverId = dropdownElement.options[dropdownElement.selectedIndex].text;
+    console.log('#############receiverId = ', receiverId);
 
-// function startGame() {
-//   pc.createOffer()
-//     .then(offer => pc.setLocalDescription(offer) )
-//     .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
-// }
+    var data = {
+        id: myId,
+        message: message
+    };
+    console.log('data = ', data);
+    document.getElementById('chat').appendChild(document.createElement('div'));
+    document.getElementById("chat").lastChild.innerHTML += myId + ': ' + message;
+    channel[receiverId].send(JSON.stringify(data));
+}
 
-// function readMessage(data) {
-
-//     var msg = JSON.parse(data.val().message);
-//     var sender = data.val().sender;
-//     if (sender != yourId) {
-//         if (msg.ice != undefined)
-//             pc.addIceCandidate(new RTCIceCandidate(msg.ice));
-//         else if (msg.sdp.type == "offer")
-//             pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-//               .then(() => pc.createAnswer())
-//               .then(answer => pc.setLocalDescription(answer))
-//               .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
-//         else if (msg.sdp.type == "answer")
-//             pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
-//     }
-// };
-
-// Offerer side
-// var channel = pc.createDataChannel("milimili");
-// channel.onopen = function(event) {
-//   channel.send('Player 1 ', yourId);
-// }
-// channel.onmessage = function(event) {
-
-//     var object = JSON.parse(event.data);
-//     console.log('A message received on Offerer side', object);
-//     if(object.id){ // this will be my logic
-//         if(object.id !== yourId){
-//             document.getElementById('chat').appendChild(document.createElement('div'));
-//             document.getElementById("chat").lastChild.innerHTML += object.id + ': ' + object.message;
-//         }
-//     }
-//     else 
-//         console.log('Player1: ', event.data);
-// }
-
-
-// // Answerer side
-// pc.ondatachannel = function(event) {
-//   var channel = event.channel;
-//   channel.onopen = function(event) {
-//     channel.send('Hi back from answerer!');
-//   }
-//   channel.onmessage = function(event) {
-
-//     var object = JSON.parse(event.data);
-//     console.log('A message received on Answerer side', object, object.id);
-//     if(object.id){ // this will be my logic
-//         if(object.id != yourId){
-//             document.getElementById('chat').appendChild(document.createElement('div'));
-//             document.getElementById("chat").lastChild.innerHTML += object.id + ': ' + object.message;
-//         }
-//     }
-//     else 
-//         console.log('Here in onmessage of Offerer', event.data);
-//   }
-// }
-
-// function chat() {
-//     var message = document.getElementById("myInput").value;
-//     var data = {
-//         id: yourId,
-//         message: message
-//     };
-//     console.log('data = ', data);
-//     document.getElementById('chat').appendChild(document.createElement('div'));
-//     document.getElementById("chat").lastChild.innerHTML += yourId + ': ' + message;
-//     channel.send(JSON.stringify(data));
-// }
-
-// // handle enter plain javascript
-// function handleEnter(e){
-//     var keycode = (e.keyCode ? e.keyCode : e.which);
-//     if (keycode == '13') {
-//       chat();
-//     }
-// }
+// handle enter plain javascript
+function handleEnter(e){
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+      chat();
+    }
+}
